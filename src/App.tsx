8c204c241aa57448ef4b1ae17a6e0ef553019c87
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-09-03 23:03:05 -0300

    feature(redux-toolkit): using another type of application as an example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { Provider as ReduxProvider } from "react-redux"
 import { store } from "./store"
-import { TodoList } from "./components/TodoList"
-import { AddTodo } from "./components/AddTodo"
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
     </ReduxProvider>
   )
 }
