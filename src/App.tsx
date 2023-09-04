349a77b4913439c2b65628195470bef8faf06a3b
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-09-03 23:19:58 -0300

    chore(redux-toolkit): installed and configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import "./styles/global.css"
+
 import { Provider as ReduxProvider } from "react-redux"
 import { store } from "./store"
+import { Player } from "./components/Page/Player"
 
 export function App() {
   return (
     <ReduxProvider store={store}>
+      <Player />
     </ReduxProvider>
   )
 }
