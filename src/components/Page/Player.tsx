212301073e9b2c6a57d658b57c1b919c123accc7
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-09-04 00:13:11 -0300

    feature(redux-toolkit): navigation styling between videos

--- src/components/Page/Player.tsx
+++ src/components/Page/Player.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, MessageCircle } from 'lucide-react'
+import { ChevronDown, MessageCircle, Video } from 'lucide-react'
 
 export function Player() {
   return (
@@ -31,6 +31,27 @@ export function Player() {
                 </div>
                 <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
               </button>
+
+              <nav className='relative flex flex-col gap-4 p-6'>
+                <button className='flex items-center gap-3 text-sm text-zinc-400'>
+                  <Video className='w-4 h-4 text-zinc-500' />
+                  <span>Fundamentos do Redux</span>
+                  <span className='ml-auto font-mono text-xs text-zinc-500'>13:25</span>
+                </button>
+
+                <button className='flex items-center gap-3 text-sm text-zinc-400'>
+                  <Video className='w-4 h-4 text-zinc-500' />
+                  <span>Fundamentos do Redux</span>
+                  <span className='ml-auto font-mono text-xs text-zinc-500'>13:25</span>
+                </button>
+
+                <button className='flex items-center gap-3 text-sm text-zinc-400'>
+                  <Video className='w-4 h-4 text-zinc-500' />
+                  <span>Fundamentos do Redux</span>
+                  <span className='ml-auto font-mono text-xs text-zinc-500'>13:25</span>
+                </button>
+              </nav>
+
             </div>
           </aside>
         </main>
