736972c7b94f34faa4d856dd97b08b27fb15d8fd
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-09-07 15:03:12 -0300

    feature(redux-toolkit): adjusting aside so that modules respect the display size and customizing scrollbar

--- src/components/Page/Player.tsx
+++ src/components/Page/Player.tsx
@@ -18,7 +18,7 @@ export function Player() {
           </button>
         </div>
 
-        <main className='relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow'>
+        <main className='relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80'>
           <div className='flex-1'>
             <div className='w-full bg-zinc-950 aspect-video'>
               <ReactPlayer
@@ -29,7 +29,8 @@ export function Player() {
               />
             </div>
           </div>
-          <aside className='w-80 border-l border-zinc-800 bg-zinc-900'>
+
+          <aside className='w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800'>
             <div>
               <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
                 <div className='flex w-10 h-10 rounded-full items-center justify-center bg-zinc-950 text-xs'>1</div>
@@ -61,6 +62,51 @@ export function Player() {
               </nav>
 
             </div>
+
+            <div>
+              <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
+                <div className='flex w-10 h-10 rounded-full items-center justify-center bg-zinc-950 text-xs'>1</div>
+                <div className='flex flex-col gap-1 text-left'>
+                  <strong className='text-sm'>Conceitos iniciais</strong>
+                  <span className='text-xs text-zinc-400'>12 aulas</span>
+                </div>
+                <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+              </button>
+
+            </div>
+
+            <div>
+              <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
+                <div className='flex w-10 h-10 rounded-full items-center justify-center bg-zinc-950 text-xs'>1</div>
+                <div className='flex flex-col gap-1 text-left'>
+                  <strong className='text-sm'>Conceitos iniciais</strong>
+                  <span className='text-xs text-zinc-400'>12 aulas</span>
+                </div>
+                <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+              </button>
+
+              <nav className='relative flex flex-col gap-4 p-6'>
+                <button className='flex items-center gap-3 text-sm text-zinc-400'>
+                  <Video className='w-4 h-4 text-zinc-500' />
+                  <span>Fundamentos do Redux</span>
+                  <span className='ml-auto font-mono text-xs text-zinc-500'>13:25</span>
+                </button>
+
+                <button className='flex items-center gap-3 text-sm text-zinc-400'>
+                  <Video className='w-4 h-4 text-zinc-500' />
+                  <span>Fundamentos do Redux</span>
+                  <span className='ml-auto font-mono text-xs text-zinc-500'>13:25</span>
+                </button>
+
+                <button className='flex items-center gap-3 text-sm text-zinc-400'>
+                  <Video className='w-4 h-4 text-zinc-500' />
+                  <span>Fundamentos do Redux</span>
+                  <span className='ml-auto font-mono text-xs text-zinc-500'>13:25</span>
+                </button>
+              </nav>
+
+            </div>
+
           </aside>
         </main>
       </div>
