7bae5553bb5184a8e1e8685e211281d7110e9ae8
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-09-09 23:12:27 -0300

    feature(redux-toolkit): loading interface created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
+import { useAppSelector } from "../store"
 import { useCurrentModuleLesson } from "../store/slices/player"
 
 export function Header() {
   const { currentModule, currentLesson } = useCurrentModuleLesson()
+  const isCourseLoading = useAppSelector((state) => state.player.isLoading)
 
-  if (!currentModule || !currentLesson) return null
+  if (isCourseLoading) {
+    return <h1 className='text-2xl font-bold'>Carregando...</h1>
+  }
 
   return (
     <div className='flex flex-col gap-1'>
-      <h1 className='text-2xl font-bold'>{currentLesson.title}</h1>
-      <span className='text-sm text-zinc-400'>Módulo {currentModule.title}</span>
+      <h1 className='text-2xl font-bold'>{currentLesson?.title}</h1>
+      <span className='text-sm text-zinc-400'>Módulo {currentModule?.title}</span>
     </div>
   )
 }
